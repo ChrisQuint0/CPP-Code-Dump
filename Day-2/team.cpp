a725eb46211268c8337e59d1a1de379f954733bd
Christopher A. Quinto <cquinto.primary@gmail.com>
2025-12-09 11:40:01 +0800

    Add Day-3 Morning Exercises
    Learned vectors and pairs. Add comments on Day-2 problems for better recall.
    
    Signed-off-by: Christopher A. Quinto <cquinto.primary@gmail.com>

--- Day-2/team.cpp
+++ Day-2/team.cpp
@@ -5,24 +5,25 @@ using namespace std;
 int main(){
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
-
+ 
+  //Get the number of programming problems
   int n;
   cin >> n;
 
-  int arr[n][3];
+  int arr[n][3]; //N number of problems, 3 person team
 
 
   int count = 0;
 
   for(int i = 0; i < n; i++){
-    int sum = 0;
+    int sum = 0; //The sum resets here
     for(int j = 0; j < 3; j++){
        cin >> arr[i][j];
-       sum += arr[i][j];
+       sum += arr[i][j]; //Add it to the sum right away
     }
     
     if(sum >= 2){
-      count++;
+      count++; //If the sum is greater than 2 then they are going to implement it, count it.
     }
   }
 
--- Day-2/watermelon.cpp
+++ Day-2/watermelon.cpp
@@ -1,5 +1,5 @@
 //Codeforces Watermelon Problem
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(){

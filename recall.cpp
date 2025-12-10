3bf9896d9cb7038244ea09269269b4d129dc0de7
Christopher A. Quinto <cquinto.primary@gmail.com>
2025-12-10 22:43:51 +0800

    Add STL algorithm examples and update recall.cpp
    Added multiple C++ source files in Day-4 demonstrating STL algorithms such as sort, find, count, accumulate, reverse, unique, and custom comparators. Updated recall.cpp to implement a solution for the 'Reviewing Presents' problem, reading input, sorting pairs, and outputting results. Corresponding compiled executables were also added.
    
    Signed-off-by: Christopher A. Quinto <cquinto.primary@gmail.com>

--- recall.cpp
+++ recall.cpp
@@ -1,4 +1,5 @@
 //A CPP file used for recoding / recalling previously solved problems
+//Reviewing Presents
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -7,6 +8,22 @@ int main(){
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
   
-  
+  int n;
+  cin >> n;
+
+  vector<pair<int, int>> v;
+
+  for(int i = 0; i < n; i++){
+    int x;
+    cin >> x;
+    v.push_back({x, i+1});
+  }
+
+  sort(v.begin(), v.end());
+
+  for(auto x: v){
+    cout << x.second << " "; 
+  }
+  cout << "\n";
   return 0;
 }

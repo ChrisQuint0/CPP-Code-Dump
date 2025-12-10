23adaf43a91e1a60ea223cbd8eafd264d9405120
Christopher A. Quinto <cquinto.primary@gmail.com>
2025-12-10 20:50:44 +0800

    Add recall cpp file and one problem solution
    I just needed a recall cpp file that I could use
    whenever I'm trying to review a solution for a
    problem.
    
    I just solved one problem today titled
    "Word Capitalization".
    
    Signed-off-by: Christopher A. Quinto <cquinto.primary@gmail.com>

---
+++ recall.cpp
@@ -0,0 +1,12 @@
+//A CPP file used for recoding / recalling previously solved problems
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  
+  
+  return 0;
+}

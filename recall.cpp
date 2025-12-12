459f2997b995f7b7e3f59434b99500894b7e8510
Christopher A. Quinto <cquinto.primary@gmail.com>
2025-12-12 14:40:35 +0800

    Add solutions for Codeforces problems and update recall.cpp
    Added new C++ source and executable files for Codeforces problems: 'Boy or Girl' (Day-5), 'Domino Piling', and 'Next Round' (Day-6). Updated recall.cpp to implement the 'Sort the arrays' problem, including logic to check if the array can be sorted by reversing a single segment.
    
    Signed-off-by: Christopher A. Quinto <cquinto.primary@gmail.com>

--- recall.cpp
+++ recall.cpp
@@ -1,29 +1,53 @@
-//A CPP file used for recoding / recalling previously solved problems
-//Reviewing Presents
-
+//Reviewing: Sort the arrays
 #include <bits/stdc++.h>
 using namespace std;
 
 int main(){
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
-  
+
   int n;
   cin >> n;
 
-  vector<pair<int, int>> v;
+  vector<int> v;
 
   for(int i = 0; i < n; i++){
     int x;
     cin >> x;
-    v.push_back({x, i+1});
+    v.push_back(x);
   }
 
-  sort(v.begin(), v.end());
+  vector<int> sv = v;
 
-  for(auto x: v){
-    cout << x.second << " "; 
+  sort(sv.begin(), sv.end());
+
+  int left = -1, right = -1;
+
+  for(int i = 0; i < n; i++){
+    if(v[i] != sv[i]){
+       if(left == -1){
+         left = i;
+       }
+
+       right = i;
+    }
   }
-  cout << "\n";
+
+  if(left == -1){
+    cout << "yes\n";
+    cout << "1 1";
+
+    return 0;
+  }
+
+  reverse(v.begin() + left, v.begin() + right + 1);
+
+  if(v == sv){
+    cout << "yes\n";
+    cout << left + 1 << " " << right + 1;
+  } else {
+    cout << "no\n";
+  }
+  
   return 0;
 }
